f54f855a54311799e228239712085e193be81a3b
apella1 <johncollons5@gmail.com>
2023-08-30 14:01:08 +0300

    Register models to admin site

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,10 @@
+"""
+Registering models to be modifiable via the admin panel
+"""
 from django.contrib import admin
+from .models import Question, Choice
 
 # Register your models here.
+
+admin.site.register(Question)
+admin.site.register(Choice)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,49 @@
-from django.db import models
+"""
+Polls models
+"""
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Question(models.Model):
+    """Question model
+
+    Args:
+        models (_type_): _description_
+    """
+
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+    # It’s important to add __str__() methods to your models,
+    # not only for your own convenience
+    # when dealing with the interactive prompt,
+    # but also because objects’ representations are used
+    # throughout Django’s automatically-generated admin.
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    """Choice model
+
+    Args:
+        models (_type_): _description_
+    """
+
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text

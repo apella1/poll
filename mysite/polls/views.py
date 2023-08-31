74d6043e8f2a5285b7686d9e9f3375e713acad79
apella1 <johncollons5@gmail.com>
2023-08-31 11:15:56 +0300

    Add loader method

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,7 +3,61 @@ Views for the polls app
 """
 
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Index")
+    """_summary_
+
+    Args:
+        request (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {"latest_question_list": latest_question_list}
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+    """_summary_
+
+    Args:
+        request (_type_): _description_
+        question_id (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    """_summary_
+
+    Args:
+        request (_type_): _description_
+        question_id (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    response = "You're looking at the results of question %s"
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    """_summary_
+
+    Args:
+        request (_type_): _description_
+        question_id (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return HttpResponse("You're voting on question %s." % question_id)

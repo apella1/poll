8635c9f8d235d629a4eaee6068c9c1c13ae1e08c
apella1 <johncollons5@gmail.com>
2023-08-31 13:24:30 +0300

    Update view templates namespaceing

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,8 +3,10 @@ Views for the polls app
 """
 
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import get_object_or_404, render
 
+# from django.http import Http404
+# from django.template import loader
 from .models import Question
 
 
@@ -18,9 +20,12 @@ def index(request):
         _type_: _description_
     """
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
+    # using loader
+    # template = loader.get_template("polls/index.html")
+    # context variable remains
     context = {"latest_question_list": latest_question_list}
-    return HttpResponse(template.render(context, request))
+    # return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
@@ -33,7 +38,13 @@ def detail(request, question_id):
     Returns:
         _type_: _description_
     """
-    return HttpResponse("You're looking at question %s." % question_id)
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    # using a shortcut for the above code
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):

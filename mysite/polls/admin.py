10d1d7a47fe8b07decf648039fe6e2698472a440
apella1 <johncollons5@gmail.com>
2023-09-08 11:37:22 +0300

    Update admin views

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -6,5 +6,23 @@ from .models import Question, Choice
 
 # Register your models here.
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    """Modifying the admin form to the desired fields
+
+    Args:
+        admin (_type_): _description_
+    """
+
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date Information", {"fields": ["pub_date"]}),
+    ]
+
+
+# class ChoiceAdmin(admin.ModelAdmin):
+#     fields = []
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
